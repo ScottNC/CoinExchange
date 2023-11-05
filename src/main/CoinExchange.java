5cd04ca4ff97f3096fa6201013579967df7db3e3
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-05 20:16:01 +0000

    Find total combinations of coins to reach target

--- src/main/CoinExchange.java
+++ src/main/CoinExchange.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 public class CoinExchange {
     public long exchange(int[] type, int n) {
         int[] descTypes = Arrays.stream(type)
-                .map(i -> -i)
+                .filter(i -> i <= n) // remove coin values larger than target as they are redundant
+                .map(i -> -i) // sorts array into descending order
                 .sorted()
                 .map(i -> -i)
                 .toArray();
@@ -11,17 +12,26 @@ public class CoinExchange {
         return findCompleteSum(descTypes, n);
     }
 
+    /*
+     Uses recursion to find total amount of combinations
+     For example if we want to find the amount of combinations to make 5 out of {1,2,3}
+     We start with 3 and have 2 to make up
+     We can run the same function to find the amount of combinations to make 2 out of {1,2}
+     */
     private long findCompleteSum(int[] type, int n) {
-
         long count = 0;
 
         for (int coin : type) {
-            int remainder = n % coin;
+            int remainder = n - coin;
+
+            int[] filteredCoins = Arrays.stream(type)
+                    .filter(i -> i <= coin)
+                    .toArray();
 
             if (remainder == 0)
                 count++;
-            else if( type.length > 1 )
-                count += findCompleteSum(Arrays.copyOfRange(type, 1, type.length), remainder);
+            else if (remainder > 0)
+                count += findCompleteSum(filteredCoins, remainder);
         }
         return count;
     }
--- src/test/CoinExchangeTest.java
+++ src/test/CoinExchangeTest.java
@@ -6,5 +6,6 @@ class CoinExchangeTest {
     void exchange() {
         CoinExchange exchanger = new CoinExchange();
         assertEquals(5, exchanger.exchange(new int[] {1,2,3},5));
+        assertEquals(4, exchanger.exchange(new int[] {1,5,10,50,100},11));
     }
 }
